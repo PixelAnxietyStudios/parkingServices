f653080753dc929efeddeb3b6a8f4f319ebe49ff
Mauricio Fortune Panizo <81243002+Mauricio-Fortune@users.noreply.github.com>
2022-02-27 17:41:51 -0500

    Create Motorcycle.h

---
+++ Motorcycle.h
@@ -0,0 +1,30 @@
+#ifndef Motorcycle_h
+#define Motorcycle_h
+
+class Motorcycle
+{
+
+  private:
+    string make;
+    string model;
+    int year;
+    double mpg;
+    int tires;
+  
+  public:
+    void setMake(string);
+    void setModel(string);
+    void setYear(int);
+    void setMPG(double);
+    void setTires(int);
+    string getMake();
+    string getModel();
+    int getYear();
+    double getMPG();
+    int getTires();
+  
+}
+
+#endif /*Motorcycle_h*/
+
+
