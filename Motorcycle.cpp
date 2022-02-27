dd253ec6c6384a0f972a96e54a1a767e8864356d
Mauricio Fortune Panizo <81243002+Mauricio-Fortune@users.noreply.github.com>
2022-02-27 17:51:40 -0500

    Create Motorcycle.cpp

---
+++ Motorcycle.cpp
@@ -0,0 +1,61 @@
+#include "Motorcycle.h"
+
+  private:
+    string make;
+    string model;
+    int year;
+    double mpg;
+    int tires;
+  
+  public:
+    void setMake(string);
+    void setModel(string);
+    void setYear(int);
+    void setMPG(double);
+    void setTires(int);
+    string getMake();
+    string getModel();
+    int getYear();
+    double getMPG();
+    int getTires();
+  
+
+void setMake(string tempMake){
+  make = tempMake;
+}
+
+void setModel(string tempModel){
+  model = tempModel;
+}
+
+void setYear(int tempYear){
+  year = tempYear 
+}
+
+void setMPG(double tempMPG){
+  mph = tempMPG; 
+}
+
+void setTires(int tempTires){
+  tires = tempTires;
+}
+
+string getMake(){
+  return make; 
+}
+
+string getModel(){
+  return model;
+}
+
+int getYear(){
+  return year;
+}
+
+double getMPG(){
+  return mpg;
+}
+
+int getTires(){
+  reutnr tires;
+}
