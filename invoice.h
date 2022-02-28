a2d3ad6ee24c4dc501b5ecaeb5a8510e6199ce08
Mauricio Fortune Panizo <81243002+Mauricio-Fortune@users.noreply.github.com>
2022-02-28 11:29:50 -0500

    Create invoice.h

---
+++ invoice.h
@@ -0,0 +1,20 @@
+#ifndef invoice_h
+#define invoice_h
+
+#include <string>
+
+class Invoice
+{
+private:
+  double price {0.0};
+  double discount {0.0};
+  double service {10.0};
+  
+public:
+  Invoice() = default;
+  Invoice(double p, double d, double s) : price{p}, discount{d}, service{s};
+  double calcTotalPrice();
+  void outputInvoice;
+};
+
+#endif /*invoice_h*/
