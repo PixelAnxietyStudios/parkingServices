c5228d0483f73de0e81e9d0506db9e67f3a603a3
ThePurd <100307938+ThePurd@users.noreply.github.com>
2022-03-02 12:00:52 -0500

    Update Visitor.cpp

--- Visitor.cpp
+++ Visitor.cpp
@@ -20,3 +20,22 @@ void Visitor::setAlumni(char a)
     if (a == 'y' || a == 'n')
         { alumni = true;}
 }
+
+string Visitor::visitorInfo(){
+  string info = "Name: ";
+  info.append(name);
+  info += "\nEmail: ";
+  info.append(email);
+  info += "\nAddress: ";
+  info.append(address);
+  info += "\nSide of Campus: ";
+  info.append(side);
+  switch (alumni){
+    case true:
+      info += "\nAlumni: yes";
+      break;
+    case false:
+      info += "\nAlumni: no";
+  }
+  return info;
+}
